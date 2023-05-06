14c518f45abe1b01db1b23c3930637384e086e24
Dani Funes <funesfuentesdaniel@gmail.com>
2023-05-06 20:03:05 +0200

    refactor: add handle errors

--- src/models/airport.js
+++ src/models/airport.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 const airportSchema = new mongoose.Schema ({
-    nombre: String,
+    nombre: {type:String , required:true, unique: true},
     distancia_ciudad: Number,
     transportes: [{type : String, enum: ['Taxi', 'Tren', 'Metro', 'Autobús']}],
     servicios: [String],
---
+++ src/models/customer.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+
+const customerSchema = new mongoose.Schema({
+  nombre: { type: String },
+  apellidos: { type: String },
+  password: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  numero_pasaporte: { type: String },
+  isAdmin: Boolean,
+  billetes: [{ type: mongoose.Schema.Types.ObjectId, ref: "Flight" }],
+});
+
+customerSchema.methods.generateJWT = function () {
+  return jwt.sign(
+    pick(this, ["nombre", "email", "isAdmin"]),
+    process.env["jwt_privateKey"]
+  );
+};
+
+const Customer = mongoose.model("Customer", customerSchema);
+
+module.exports = Customer;
